bf3509b8e7e11afd4666a5507f9ff660d25e709d
Teperi Lee <teperi31@gmail.com>
2019-02-25 15:08:43 +0900

    Nomad Coders 강의 완료
    step6.md 내용 추가(Promise 수정 / Async Awiat 추가)
    yarn 을 통한 github Pages 등록
    영화 리스트에 시놉시스 및 장르 분류 추가
    추후 CSS 작업으로 완료 예정

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -4,30 +4,46 @@ import Movie from './Movie';
 
 class App extends Component {
 
-  state = {
-  }
+    state = {}
 
-  componentDidMount(){
-    fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
-  }
+    componentDidMount() {
+        this._getMovies();
+    }
 
-  _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index}/>
-    })
-    return movies;
-  }
+    _renderMovies = () => {
+      
+        const movies = this.state.movies.map((movie, index) => {
+          console.log(movie)
+            return <Movie
+            title = { movie.title }
+            poster = { movie.medium_cover_image }
+            genres = {movie.genres}
+            synopsis = {movie.synopsis}
+            key = { movie.id }/>
+        })
+        return movies;
+    }
 
-  render() {
-    return (
-      <div className = "App" >
-        {this.state.movies ? this._renderMovies() : "Loading"}
-      </div>
-    );
-  }
+    _getMovies = async () => {
+      const movies = await this._callApi()
+      this.setState({
+        movies
+      })
+    }
+
+    _callApi = () => {
+      // api 에서 받아오는 객체가 promise 로 넘어옴
+      return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
+      .then(response => response.json()) // 받아온 객체를 JSON 으로 바꾼 후
+      .then(json => json.data.movies) // 이후 console.log
+      .catch(err => console.log(err)) // error 가 생기면 여기서 처리
+    }
+
+    render() {
+      
+        return ( <div className = "App" > { this.state.movies ? this._renderMovies() : "Loading" } </div>
+        );
+    }
 }
 
 export default App;
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,29 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LineEllipsis from 'react-lines-ellipsis';
 import './Movie.css';
 
-function Movie({title,poster}) {
-    return (
-        <div>
-                <h1>{title}</h1>
-                <MoviePoster poster={poster}/>
+function Movie({ title, poster, genres, synopsis }) {
+    return ( 
+        <div className="Movie">
+            <div className="Movie__Columns">
+                <MoviePoster poster = { poster } alt={title}/> 
             </div>
+            <div className="Movie__Columns">
+                <h1 > { title } </h1> 
+                <div className="Movies_Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index}/>)}
+                </div>
+                <p className="Movie__Synopsis">
+                    <LineEllipsis
+                        text={synopsis}
+                        maxLine='2'
+                        ellipsis='...'
+                        trimRight
+                        basedOn='letters'
+                    />
+                </p>
+            </div>
+        </div>
     )
 }
 
-function MoviePoster({poster}){
-    return(
-        <img src={poster} alt=""/>
+function MoviePoster({ poster, alt }) {
+    return ( 
+        <img src = { poster } alt={alt} className="Movie__Poster"/>
     );
+}
+
+function MovieGenre({genre}){
+    return ( 
+        <span className="Movie__Genre">{genre} </span> 
+    )
 }
 
 Movie.propTypes = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
 }
 
-MoviePoster.propTypes={
-    poster: PropTypes.string.isRequired
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired
 }
 
 export default Movie;
